422e3c1b8d93b81be930f2d2b98501220cc5a163
Sem4kok <w1usis@mail.ru>
2024-04-15 15:59:55 +0300

    feat(server): +getAlbums. Handler struct

--- api/server.go
+++ api/server.go
@@ -6,34 +6,47 @@ import (
 	"github.com/Sem4kok/restful-api/db"
 	"github.com/Sem4kok/restful-api/util"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"log"
+	"net/http"
 )
 
 const (
 	HOST = "localhost:8080"
 )
 
+type Handler struct {
+	Conn *pgx.Conn
+}
+
 var albums []util.Album
 
 func StartServer() {
 
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-
-	conn := db.StartDBConnection()
+	handler := &Handler{Conn: db.StartDBConnection()}
 	defer func() {
-		_ = conn.Close(context.Background())
+		_ = handler.Conn.Close(context.Background())
 	}()
 
-	albums, err := db.GetAlbumsFromDB(context.Background(), conn)
+	router := gin.Default()
+	router.GET("/albums", handler.getAlbums)
+
+	err := router.Run(HOST)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// private method of Handler struct
+// that implements Get Method
+func (h *Handler) getAlbums(c *gin.Context) {
+	var err error = nil
+	albums, err = db.GetAlbumsFromDB(context.Background(), h.Conn)
 	if err != nil && albums == nil {
 		log.Fatal(err)
 	} else if err != nil {
 		fmt.Println("error, with rows.Err(): ", err.Error())
 	}
 
-	err = router.Run(HOST)
-	if err != nil {
-		log.Fatal(err)
-	}
+	c.IndentedJSON(http.StatusOK, albums)
 }
