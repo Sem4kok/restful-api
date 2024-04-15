06f6b59e497fac0195b5345b961a0358d3fb09de
Sem4kok <w1usis@mail.ru>
2024-04-15 15:16:57 +0300

    fix(db): added local connection to db

--- db/initDB.go
+++ db/initDB.go
@@ -4,19 +4,21 @@ import (
 	"context"
 	"github.com/jackc/pgx/v5"
 	"log"
-	"os"
 )
 
 func StartDBConnection() *pgx.Conn {
 
 	// you must set url of your database. syntax allowed :
 	// postgresql://user:password@localhost:5432/database
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL == "" {
-		log.Fatal("DATABASE_URL environment is non set")
-	}
 
-	conn, err := pgx.Connect(context.Background(), "postgresql://postgres:Pagany93FZonda!@localhost:5432/matmex")
+	/*
+		dbURL := os.Getenv("DATABASE_URL")
+		if dbURL == "" {
+			log.Fatal("DATABASE_URL environment is non set")
+		}
+	*/
+
+	conn, err := pgx.Connect(context.Background(), "postgresql://postgres:SuperSmen@localhost:5432/matmex")
 	if err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/Sem4kok/restful-api/db"
+
 func main() {
+	db.StartDBConnection()
 }
